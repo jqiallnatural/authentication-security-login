59b278d5bfb1141e00bb60aaad176317963a7344
Jiaqi Zhao <jiaqi.zhao.tr@gmail.com>
2020-07-04 14:51:43 -0400

    ✨ Implement user register and login

--- app.js
+++ app.js
@@ -13,12 +13,20 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
  
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true } )
+
+const userSchema = {
+  email: String,
+  password: String
+}
+
+const User = new mongoose.model("User", userSchema)
+
 app.get('/', (req, res) => {
   res.render('home');
 });
 
-mongoose.connect("mongodb://localhost:27017/userDB", {userNewUrlParser: true})
- 
+
 app.get('/login', (req, res) => {
   res.render('login');
 });
@@ -26,6 +34,38 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
   res.render('register');
 });
- 
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  })
+
+  newUser.save(function(err){
+    if (err) {
+      console.log(err)
+    } else {
+      res.render("secrets")
+    }
+  })
+})
+
+app.post("/login", function(req, res){
+  const username = req.body.username
+  const password = req.body.password
+
+  User.findOne({email: username}, function(err, foundUser){
+    if (err) {
+      console.log(err)
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets")
+        }
+      }
+    }
+  })
+})
+
 app.listen(port, () => console.log(`Server started at port: ${port}`)
 );
